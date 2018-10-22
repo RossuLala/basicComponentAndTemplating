3e763a8359ab9fd97490148ef97703e5711545bd
RossuLala <lala@intralog.hu>
2018-10-22 15:21:36 +0200

    *ngFor - tömb megjelenítése

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basicComponentAndTemplating';
+  title = '---basicComponentAndTemplating';
+  events = [
+    'sziget',
+    'efot',
+    'fazeb',
+    'szin'
+  ];
 }
